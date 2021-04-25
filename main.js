cdd68a982712a180b9249d7f30693cf4aa8e6711
hortencia718 <hortenciaecisneros@gmail.com>
2021-04-25 14:26:05 -0400

    finished project

--- main.js
+++ main.js
@@ -8,20 +8,20 @@ btns.forEach(function(btn){
         const color = e.currentTarget.classList;
 
         if(color.contains("btn1")){
-           theme.style.setProperty("--theme-color", "rgb 79, 236, 65");
+           theme.style.setProperty("--theme-color", "#ff1717");
         }
         else if(color.contains("btn2")){
-            theme.style.setProperty("--theme-color", "rgb 192, 29, 40");
+            theme.style.setProperty("--theme-color", "#4eeb8f");
         }
         else if(color.contains("btn3")){
-            theme.style.setProperty("--theme-color", "rgb 255, 215, 105");
+            theme.style.setProperty("--theme-color", "#f7c44c");
         }
         else if(color.contains("btn4")){
-            theme.style.setProperty("--theme-color", "rgb 96, 34, 220");
+            theme.style.setProperty("--theme-color", "#7c1cd1");
 
         }
         else{
-            theme.style.setProperty("--theme-color", "rgb 44, 167, 220");
+            theme.style.setProperty("--theme-color", "#4ec6eb");
 
         }
     });
